b15a35ead55edfdad5548d5157431f7d214da3f7
hungnguyend <dinhhungonline@gmail.com>
2022-11-06 18:36:09 +0700

    Add EF entities and DbContext

---
+++ vShop.Data/Configuration/ProductConfiguration.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using vShop.Data.Entities;
+using vShop.Data.Entities.Enum;
+
+namespace vShop.Data.Configuration
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.ToTable("Product");
+
+            builder.HasKey(t => t.Id);
+
+            builder.Property(t => t.Name).IsRequired().HasMaxLength(250);
+            
+            builder.Property(t => t.Desc).HasMaxLength(400);
+
+            builder.Property(t => t.Price).IsRequired();
+
+            builder.Property(t => t.CategoryId).IsRequired(false);
+
+            builder.Property(t => t.Status).HasDefaultValue(Status.Active);
+
+            builder.HasOne(t => t.Category)
+                .WithMany(t => t.Products)
+                .HasForeignKey(t => t.CategoryId)
+                .IsRequired(false)
+                .HasConstraintName("FK_Product_Category")
+                .OnDelete(DeleteBehavior.SetNull);
+
+        }
+    }
+}
---
+++ vShop.Data/Entities/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using vShop.Data.Entities.Enum;
+
+namespace vShop.Data.Entities
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Desc { get; set; }
+        public string Image { get; set; }
+        public decimal Price { get; set; }
+        public decimal OriginalPrice { get; set; }
+        public Guid? CategoryId { get; set; }
+        public Status Status { get; set; }
+        public virtual Category? Category { get; set; }
+
+    }
+}
